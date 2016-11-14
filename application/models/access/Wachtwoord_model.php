2515b6ad44e9b59580b130709a327b18cc78ad3d
Jorrit <Edwin@192.168.16.38>
2016-11-14 12:36:37 +0100

    cleaned my code does it work?

--- application/models/access/Wachtwoord_model.php
+++ application/models/access/Wachtwoord_model.php
@@ -17,11 +17,11 @@ class Wachtwoord_model extends CI_Model {
     
     public function store_password($wachtwoord_data) {
         
-        $data_send = $this->db->insert('user_login', $wachtwoord_data);
+        //$data_send = $this->db->insert('user_login', $wachtwoord_data);
         
-        $user_id = $this->session->userdata('user_id');
+        //$user_id = $this->session->userdata('user_id');
         
-        $this->db->where('user_id', $user_id);
+        $this->db->where('user_id', $wachtwoord_data);
         
         $pass_send = $this->db->update('user_login' ,$wachtwoord_data);
         
