f778ba340e6af890ed7428a2b3b624be694967c7
Jorrit <Edwin@192.168.16.38>
2016-11-16 10:35:49 +0100

    forgot ww?

--- application/views/login/vergeten.php
+++ application/views/login/vergeten.php
@@ -1,3 +1,37 @@
+<script type="text/javascript">
+
+    $(function () {
+        $("#form").submit(function (event) {
+            $("#submitForm").attr("disabled", true);
+            event.preventDefault();
+            var form = $("#form").serialize();
+            var site_url = "<?php echo site_url("login/"); ?>";
+//            var pw = $("#password").val();
+//            var repeatpw = $("#repeatpassword").val();
+            console.log(form);
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                data: form
+            })
+                    .done(function (json) {
+                       console.log(json);
+                       $('#title').html("Melding :");
+                       $('#result').html(json.response);
+                       $('#popup').modal('show');
+                       if (json.response === "Wachtwoord gemaakt.") location.href = site_url;
+                    }) 
+                    .fail(function (error) {
+                        $('#title').html("Melding :");
+                        $('#result').html("error:" +error);
+                        $('#popup').modal('show');
+                    })
+                    .always(function () {
+                        $("#submitForm").attr("disabled", false);
+                    });
+        });
+    });
+</script>
 <div class="top-content">
     <div class="inner-bg">
         <div class="container">
@@ -22,24 +56,38 @@
                             <i class="fa fa-lock"></i>
                         </div>
                     </div>
-                    <!--Hier word de Code die de user heeft gekregen gebruikt om het account te activeren.-->
+                    <!--Here you can set a new password.-->
                     <div class="form-bottom">
-                        <form class="register-form" id="send" method="POST">
+                        <form class="register-form" name="form" id="form" method="POST">
                             <div class="form-group">
                                 <input type="text" name="emailaddress" required placeholder="uwemailaddress@voorbeeld.com..." class="form-control input-lg">
                             </div>
-                            <div class="form-group">
-                                <input type="password" name="newpassword" required placeholder="Old Password..." class="form-control input-lg">
+<!--                            <div class="form-group">
+                                <input type="password" name="oldpassword" required placeholder="current Password..." class="form-control input-lg">
                             </div>
                             <div class="form-group">
-                                <input type="password" name="repeatpassword" required placeholder="New Password..." class="form-control input-lg">
-                            </div>
-                            <button type="submit" class="btn btn-danger btn-block btn-lg" id="login_button">Verbeter Password.</button>
+                                <input type="password" name="newpassword" required placeholder="New Password..." class="form-control input-lg">
+                            </div>-->
+                            <button type="submit" id="submitForm" class="btn btn-danger btn-block btn-lg" id="login_button">Verbeter Password.</button>
+                            <?php echo add_csrf_token();?>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
+<div class="modal fade" id="popup" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header" id="title" style="color: red">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="result" style="color: red">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
