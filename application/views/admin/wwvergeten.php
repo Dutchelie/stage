e0c06ac8444fa88c37182c10171cd29e43b97ad7
Jorrit <Edwin@192.168.16.38>
2017-01-12 09:59:02 +0100

    test

--- application/views/admin/wwvergeten.php
+++ application/views/admin/wwvergeten.php
@@ -7,16 +7,17 @@
             <div class="modal-dialog modal-sm">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4>Hi. <?php //echo $h4 ?></h4>
+                        <h4>Hi <?php echo $h4 ?>.</h4>
                     </div>
                     <div class="modal-body">
                         Klik op de link hieronder om uw nieuwe wachtwoord te maken.
                     </div>
                     <div class="modal-footer">
-                        <?php 
-                            //echo $encryptcode
+                        <?php
+                        $this->session->userdata("encryptcode");
+                        //echo $this->session->userdata("device"); 
                         ?>
-                        <a href="<?php echo site_url('herstel/?hash='. $encryptcode)?>" class="btn btn-primary">Confirm Now</a>
+                        <a href="<?php echo site_url('wachtwoord/?hash=' . $this->session->userdata("encryptcode")) ?>" class="btn btn-primary">Confirm Now</a>
                     </div>
                 </div>
             </div>
