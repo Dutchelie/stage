92b1d73856df9efd1da3ce6303c81c587868f234
Jorrit <Edwin@192.168.16.38>
2017-01-12 09:58:54 +0100

    test

--- application/views/admin/emailview.php
+++ application/views/admin/emailview.php
@@ -7,18 +7,17 @@
             <div class="modal-dialog modal-sm">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4>Hi <?php echo $h4 ?></h4>
+                        <h4>Hi <?php echo $h4 ?>.</h4>
                     </div>
                     <div class="modal-body">
-                        This is your final step to complete your registration.
-                        Please confirm you email address by clicking on the button below.
+                        Dit is de laatste stap voor het maken van uw account.
                     </div>
                     <div class="modal-footer">
                         <?php 
-                            echo $encryptcode
+                            $this->session->userdata("encryptcode");
                         ?>
                         
-                        <a href="<?php echo site_url('wachtwoord/?hash='. $encryptcode)?>" class="btn btn-primary">Confirm Now</a>
+                        <a href="<?php echo site_url('wachtwoord/?hash='. $this->session->userdata("encryptcode"))?>" class="btn btn-primary">Confirm Now</a>
                     </div>
                 </div>
             </div>
