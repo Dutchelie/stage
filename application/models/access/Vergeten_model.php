5acded7afe675d44faed903955fac0495457d960
Jorrit <Edwin@192.168.16.38>
2017-01-12 09:58:35 +0100

    test

--- application/models/access/Vergeten_model.php
+++ application/models/access/Vergeten_model.php
@@ -1,28 +1,30 @@
 <?php
-
+//models only receive and retreive data away
 class Vergeten_model extends CI_Model {
-    
-    public function data_check($data_check) {
-        
+    //checks if email already exists in database.
+    public function data_get($user_email) {
+        //from table user_login
         $this->db->from('user_login');
-        
-        $this->db->where($data_check);
-        
-        $query = $this->db->get();
-        $check_hash = $query->row_array();
-        
-        return $check_hash;
-    }
-    
-    public function change_pw($user_email, $user_pw) {
-        
-        $this->db->from('user_login');
-        
+        //where array $user_email = username => (emailaddress) test@gmail.com
         $this->db->where($user_email);
-        
-        $updated_pw = $this->db->update('user_login',$user_pw);
-        
-        return $updated_pw;
-        
+        //if the emailaddress test@gmail.com exists in the column emailaddress
+        //retreive the whole row of that (value) test@gmail.com and make an array of it
+        $query = $this->db->get();
+        $id = $query->row_array();
+        //only send the user_id of the array to the controller
+        return $id["user_id"];
+    }
+    //get the users name for the email
+    public function get_name ($mail) {
+        //from the table user
+        $this->db->from('user');
+        //where array $mail = emailaddres => (emailaddress) test@gmail.com
+        $this->db->where($mail);
+        //if the emailaddress test@gmail.com exists in the column emailaddress
+        //retreive the whole row of that (value) test@gmail.com and make an array of it
+        $query = $this->db->get();
+        $firstname = $query->row_array();
+        //only send the firstname of the array to the controller
+        return $firstname["firstname"];
     }
 }
